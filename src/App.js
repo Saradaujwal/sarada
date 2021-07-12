e9590252f0a582e1f54d5f8ec6bbd34697cc5d3b
Saradaujwal <86129176+Saradaujwal@users.noreply.github.com>
2021-07-12 14:35:57 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello Sarada </h1>
+      <h2>Welcome to online Grocery store!</h2>
+    </div>
+  );
+}
